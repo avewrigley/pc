21826eb6e4c8f486474f426c50249ce1b3de67a0
Ave Wrigley <ave.wrigley@ave.wrigley.name>
2022-08-29 21:10:52 +0100

    deprecate bank_days

--- pc.py
+++ pc.py
@@ -16,25 +16,25 @@ def read_bank_file():
     """
     if exists(BANK_FILE):
         with open(BANK_FILE, 'r', encoding="UTF-8") as filehandle:
-            bank_days = int(filehandle.readline())
             bank = int(filehandle.readline())
+            print(bank)
             rest_days_used = int(filehandle.readline())
+            print(rest_days_used)
             start_day_str = filehandle.readline()
             if start_day_str:
                 start_day = datetime.strptime(start_day_str, "%d/%m/%Y")
             else:
                 start_day = None
-            return bank_days, bank, rest_days_used, start_day
-    return 0, 0, 0, None
+            return bank, rest_days_used, start_day
+    return 0, 0, None
 
 
-def write_bank_file(days, bank, rest_days_used, start_day):
+def write_bank_file(bank, rest_days_used, start_day):
     """
     cache bank values to file
     """
     with open(BANK_FILE, 'w', encoding="UTF-8") as filehandle:
         filehandle.write('\n'.join((
-            str(days),
             str(bank),
             str(rest_days_used),
             start_day.strftime('%d/%m/%Y')
@@ -90,7 +90,7 @@ def main():
         help='resigster rest day'
     )
     args = parser.parse_args()
-    bank_days, bank, rest_days_used, start_day = read_bank_file()
+    bank, rest_days_used, start_day = read_bank_file()
     if args.start_day:
         start_day = datetime.strptime(args.start_day, "%d/%m/%Y")
     assert start_day, "start day set"
@@ -99,26 +99,24 @@ def main():
     rest_days = calc_rest_days(start_day)
     todo = days * 5
     count = args.count or 0
+    credit = count - todo
 
     if args.bank is not None:
         bank = args.bank
-        bank_days = 0
     if args.rest_day:
         rest_days_used = rest_days_used + 1
     elif not args.start_day:
-        if bank_days != days:
-            credit = count - todo
-            bank = bank + credit
+        bank = bank + credit
     rest_days_delta = rest_days - rest_days_used
     print(
         f'start_day={start_day},\
 todo={todo},days={days},\
 rest_days_delta={rest_days_delta},\
 count={count},\
-bank={bank}'
+bank={bank}',
     )
-    print('/'.join((str(count), str(rest_days_delta), str(bank))))
-    write_bank_file(days, bank, rest_days_used, start_day)
+    print('/'.join((str(todo), str(rest_days_delta), str(bank))))
+    write_bank_file(bank, rest_days_used, start_day)
 
 
 if __name__ == "__main__":
